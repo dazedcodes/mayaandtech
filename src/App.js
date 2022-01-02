e60b95f5b76b502c3d06e63876c956571dbbbeec
dazedcodes <techwithmaya@gmail.com>
2022-01-02 16:02:47 -0600

    fixing homepage

--- src/App.js
+++ src/App.js
@@ -9,9 +9,6 @@ import NavBar from './components/NavBar';
 import Footer from "./components/Footer";
 import VeryFunnyDad from './components/VeryFunnyDad';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 
 function App() {
   return (
@@ -34,22 +31,5 @@ function App() {
     </Router>
   );
 }
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDpKKT5f0Dkz9J6odgOTJqWmAmQ7L-WTwA",
-  authDomain: "mayaandtechwebsite.firebaseapp.com",
-  projectId: "mayaandtechwebsite",
-  storageBucket: "mayaandtechwebsite.appspot.com",
-  messagingSenderId: "894047096485",
-  appId: "1:894047096485:web:7e5521ab71b118ea32874c",
-  measurementId: "G-4M628RGDNB"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 export default App;
