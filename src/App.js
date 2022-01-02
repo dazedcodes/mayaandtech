3c598900a46c90d39f3aa221d1908a2be5c03fbc
dazedcodes <techwithmaya@gmail.com>
2022-01-02 16:46:11 -0600

    fixing build errors

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Projects from './components/Projects';
 import Motto from './components/Motto';
 import Experience from './components/Experience';
 import Contact from './components/Contact';
-import NavBar from './components/NavBar';
 import Footer from "./components/Footer";
 import VeryFunnyDad from './components/VeryFunnyDad';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
