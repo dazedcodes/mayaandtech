d9cd4db35aca1d808076598ec069527c6e369f88
dazedcodes <techwithmaya@gmail.com>
2022-01-03 14:27:48 -0600

    Update: AboutMe container, Experiences width, and heading for Motto page

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -7,12 +7,17 @@ import '../App.css';
 export default function AboutMe() {
     return (
         <div className="aboutme parallax">
-            <img className="aboutmeProfilePic"  alt="A headshot of Maya Murphy smiling." src={mayaProfile}/>
-            <PageHeader 
-                title="About Me"
-                subtitle="A LENS THAT HELPS ONE SEE CLEARLY"
-                paragraph="At an early age, I was always cognizant of my figure it out mentality, a worldview developed from the reoccurring instability endured in my childhood. Nevertheless, crafting solutions with grace has proven to provide clarity for myself and all those whom I cross paths with."
-            />
+            <div>
+                <img className="aboutmeProfilePic"  alt="A headshot of Maya Murphy smiling." src={mayaProfile}/>
+            </div>
+            <div>
+                <PageHeader 
+                    title="About Me"
+                    subtitle="A LENS THAT HELPS ONE SEE CLEARLY"
+                    paragraph="At an early age, I was always cognizant of my figure it out mentality, a worldview developed from the reoccurring instability endured in my childhood. Nevertheless, crafting solutions with grace has proven to provide clarity for myself and all those whom I cross paths with."
+                />
+            </div>
+            
             
         </div>
     )
--- src/components/Motto.js
+++ src/components/Motto.js
@@ -1,8 +1,13 @@
-import React from 'react'
+import React from 'react';
+import PageHeader from './PageHeader';
 
 export default function Motto() {
     return (
         <div className="motto parallax">
+            <PageHeader 
+                title="My Life Motto"
+                subtitle="THAT KEEPS ME SANE"
+            />
             <p className="quote">You have power over your mind - not outisde events. Realize this, and you will find strength.</p>
             <hr/>
             <p className="author">Marcus Aurelius</p>
